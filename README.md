027f6532823d8b4ea9e7ee1c868746a01b7631a4
宮城 和音 <kazune.miyagi@bizreach.co.jp>
2018-11-30 19:32:29 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# PythonLoadTest
+# Python Load Test
+
+## Requirement Packages
+```
+$ python3 -m pip install locustio
+$ brew install libev
+$ pip install mysqlclient
+# $ brew install mysql-connector-c
+```
+
+## How to up
+```
+$ docker-compose up -d
+$ mysql -P 3306 -h 127.0.0.1 -uroot
+```
---
+++ mysql_connection_test.py
@@ -0,0 +1,28 @@
+import MySQLdb
+
+conn = MySQLdb.connect(
+    user='root',
+    passwd='',
+    host='127.0.0.1',
+    db='sample',
+    port=3306
+)
+
+
+# カーソルを取得する
+cur = conn.cursor()
+
+# 初期化
+cur.execute("delete from SYNONYM_RELATIONS;")
+conn.commit()
+
+# Insert実行
+for i in range(1000):
+    cur.execute("insert into SYNONYM_RELATIONS values (%s, %s);", ("Synonym" + str(i), "Representative" + str(i)))
+
+conn.commit()
+
+cur.execute("select * from SYNONYM_RELATIONS;")
+print(cur.rowcount)
+# print(cur.fetchall())
+conn.close()
